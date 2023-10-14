9131c1501b98644806338b5324b4c1f8f0db5c78
certainty <david.krentzlin@gmail.com>
2023-10-14 16:15:07 +0200

    Scan identifiers

--- src/compiler/frontend/scanner.lisp
+++ src/compiler/frontend/scanner.lisp
@@ -4,12 +4,13 @@
   ((input :initarg :input :type source-input :reader scanner-input :documentation "The input to scan")
    (errors :initform nil :type  list :accessor scanner-errors :documentation "A list of errors encountered during scanning")
    (line :initform 1 :type integer  :accessor scanner-line :documentation "The current line number")
-   (column :initform 0 :type integer :accessor scanner-column :documentation "The current column number")))
+   (column :initform 1 :type integer :accessor scanner-column :documentation "The current column number")
+   (offset :initform 0 :type integer :accessor scanner-offset :documentation "The offset in the input")))
 
 (defmethod print-object ((s scanner) stream)
   (print-unreadable-object (s stream :type t :identity t)
-    (with-slots (line column errors) s
-      (format stream "line:~a column:~a errors: ~a" line column errors))))
+    (with-slots (line column offset errors) s
+      (format stream "line:~a column:~a offset:~a errors: ~a" line column offset errors))))
 
 (defgeneric create-scanner (input)
   (:documentation "Creates a new scanner for the given input"))
@@ -24,8 +25,14 @@
 This operation always succeeds unless a condition is raised.
 If the input isn't recognized we simply return the special failure token and add the error to the internal scanner state.
 "
+  (skip-whitespaces scanner)
+  (when (eof-p scanner)
+    (return-from next-token (make-token :type +token-eof+)))
 
-  (make-token :type +token-illegal+))
+  (let ((next (peek scanner)))
+    (cond
+      ((or (alpha-char-p next) (char= #\_ next)) (scan-identifier scanner))
+      (t (make-token :type +token-illegal+)))))
 
 (defun eof-p (scanner)
   "Returns true if the scanner has reached the end of the input"
@@ -47,6 +54,7 @@ If the input isn't recognized we simply return the special failure token and add
    Returns the character that was advanced to or nil if the end of the input has been reached."
   (let ((current-char (read-char (source-input-stream (scanner-input scanner)) nil)))
     (incf (scanner-column scanner))
+    (incf (scanner-offset scanner))
     (when (eql current-char #\Newline)
       (incf (scanner-line scanner))
       (setf (scanner-column scanner) 0))
@@ -57,3 +65,31 @@ If the input isn't recognized we simply return the special failure token and add
   "Peeks at the next character in the input stream without advancing the scanner."
   (let ((stream (source-input-stream (scanner-input scanner))))
     (peek-char nil stream nil nil)))
+
+(-> location (scanner) source-location)
+(defun location (scanner)
+  (with-slots (column line offset) scanner
+    (make-source-location :line line :column column :offset offset)))
+
+(-> scan-identifier (scanner) token)
+(defun scan-identifier (scanner)
+  (let ((consumed (scan-while scanner #'identifier-char-p))
+        (loc (location scanner)))
+    (if (null consumed)
+        (make-token :type +token-illegal+)
+        (make-token :type +token-identifier+ :text (coerce consumed 'string) :location loc))))
+
+(defun identifier-char-p (c)
+  (or (sb-unicode:digit-value c) (sb-unicode:alphabetic-p c) (char= c #\_)))
+
+(defun advance-if (scanner predicate)
+  (when (funcall predicate (peek scanner))
+    (advance! scanner)))
+
+(defun scan-while (scanner predicate)
+  (let ((consumed '()))
+    (loop
+      (if (eof-p scanner)
+          (return (nreverse consumed))
+          (let ((next (advance-if scanner predicate)))
+            (if next (push next consumed) (return (nreverse consumed))))))))
