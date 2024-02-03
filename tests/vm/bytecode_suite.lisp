fc86c8af9d8be1eba2ea35fa9860f2e3031b72e8
certainty <david.krentzlin@gmail.com>
2024-02-03 16:13:55 +0100

    Disable the last remaining snapshot test

--- tests/vm/bytecode_suite.lisp
+++ tests/vm/bytecode_suite.lisp
@@ -1,6 +1,9 @@
 (in-package :tests.vm.bytecode)
 
-(define-test disassembler-works ()
-  (snapshots:assert-snapshot-equals
-   "disassembler.snapshot"
-   (with-output-to-string (s) (bytecode:disass (compiler:compile "3 + 3") :stream s))))
+;; Disabled since the result of chunks isn't deterministic
+;;
+;; (define-test disassembler-works ()
+;;   (snapshots:assert-snapshot-equals
+;;    "disassembler.snapshot"
+;;    (with-output-to-string (s) (bytecode:disass (compiler:compile "3 + 3") :stream s)))
+;;   )
